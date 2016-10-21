e953cd40c7a07bd2fe779de52c9e73b41c65a221
chrisemery <chris.emery@cpcc.edu>
2016-10-21 18:23:27 -0400

    preliminary changes to work with new version (12.1) of BMC footprints

--- __init__.py
+++ __init__.py
@@ -4,11 +4,14 @@ Used to connect to BMC (Numara) Footprints SOAP API.
 Usage:
     import footprints
     con = footprints.Connection('username', 'host.example.com', password='mypassword')
-    issue = con.getIssue(1, 536354) # where 1 is the project id, and 536354 is the issue id
+    issue = con.getIssue(1, 536354) # where 1 is the item definition id, and 536354 is the item id
     print issue
 """
-import urllib, urllib2, getpass
+import urllib, urllib2, getpass, requests
 from xml.etree import ElementTree as ET
+from zeep import Client
+from zeep.transports import Transport
+from requests.auth import HTTPBasicAuth
 
 class Connection(object):
     """
@@ -17,65 +20,45 @@ class Connection(object):
     Expects a userid and fqdn to setup the connection.
 
     """
-    def __init__(self, userid, webhost, ssl=True, password = None):
+    def __init__(self, userid, webhost, password = None):
         if not password:
             self.pword = getpass.getpass()
         else:
             self.pword = password
         self.user = userid
-        if ssl:
-            protocol = 'https'
-        else:
-            protocol = 'http'
-        self.url = "%s://%s/MRcgi/MRWebServices.pl" % (protocol, webhost)
+        self.url = "https://{}/footprints/servicedesk/externalapisoap/ExternalApiServicePort?wsdl".format(webhost)
+        self.client = Client(self.url,
+                             transport=Transport(http_auth=HTTPBasicAuth(self.user, self.pword)))
 
-    def getIssue(self, project_id, issue_id):
+    def getIssue(self, definition_id, issue_id):
         """
         Retrieve information about an issue from Footprints via a SOAP API call.
 
-        Accepts two int arguments, projectid and issue_id.
         """
-        #TODO: Move some of this boilerplate out of method for reuse in other API actions
-        data = """
-            <SOAP-ENV:Envelope 
-              xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" 
-              xmlns:namesp2="http://xml.apache.org/xml-soap" 
-              xmlns:xsd="http://www.w3.org/2001/XMLSchema" 
-              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-              xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/">
-                <SOAP-ENV:Header/>
-                <SOAP-ENV:Body> 
-                    <namesp1:MRWebServices__getIssueDetails xmlns:namesp1="MRWebServices">
-                        <user xsi:type="xsd:string">%s</user> 
-                        <password xsi:type="xsd:string">%s</password> 
-                        <extrainfo xsi:type="xsd:string"/> 
-                        <projectnumber xsi:type="xsd:int">%d</projectnumber> 
-                        <mrid xsi:type="xsd:int">%d</mrid> 
-                    </namesp1:MRWebServices__getIssueDetails> 
-                </SOAP-ENV:Body> 
-            </SOAP-ENV:Envelope>
-                        """ % (self.user, self.pword, project_id, issue_id)
+        fields = ['Title', 'Updated On', 'Created On', 'Status','Email Address']
+        def_id = definition_id
+        item_id = issue_id
+        res = self.client.service.getItemDetails({'_itemDefinitionId':def_id,'_itemId':item_id,'_fieldsToRetrieve':fields })
+        #return res
+        return self.extract_info(res)
 
-        data8 = data.encode('utf-8')
-        headers = {
-           "SOAPAction" : "MRWebServices#MRWebServices__getIssueDetails",
-           "Content-Type" : 'text/xml; charset=utf-8',
-           "Content-Length" : "%d" % len(data8)
-           }
-        options = {
-          "method" : "post",
-        }
-        req = urllib2.Request(self.url, data=data8, headers=headers)
-        try: 
-            resp = urllib2.urlopen(req)
-            resp_xml = ET.fromstring(resp.read())
-            top = resp_xml[0][0][0]
-            issue = {}
-            for item in top.getchildren():
-                if item.text: 
-                    issue[item.tag] = item.text
-            issue['raw'] = top
-            return issue
-        except urllib2.HTTPError as e:
-            return e.fp.readlines()
-
+    def extract_info(self, results):
+        """
+        """
+        pretty_result = {}
+        fields = ['Title', 'Updated On', 'Created On', 'Status','Email Address']
+        for item in results['_itemFields']['itemFields']:
+            print item
+            try:
+                label = item['fieldName']
+                value = item['fieldValue']['value']
+                if label in fields:
+                    if (type(value) is list and len(value) == 1):
+                        pretty_result[label] = value[0]
+                    else:
+                        pretty_result[label] = value
+                else:
+                    pass
+            except TypeError:
+                pass
+        return pretty_result
